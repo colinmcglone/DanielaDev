338e8b61468d65e06fb3ee3528e180b379e0b84c
colinmcglone <me@colinmcglone.ca>
2019-03-05 13:31:25 -0500

    initialise s

--- colin_double_linked_list.c
+++ colin_double_linked_list.c
@@ -125,7 +125,7 @@ int main() {
   struct cell *dave = insert(7, test, NULL);
   struct cell *vic = insert(3, test, dave);
 
-  s = search(3);
+  struct cell *s = search(3);
   walk(s);
 
   free(test);
